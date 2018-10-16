f971e824f15811b9834d54825066cb2e18a2f859
tatsumack <tatsu.mack@gmail.com>
2018-10-16 15:37:16 +0900

    refactor 1_12

--- src/chapter1/1_12.go
+++ src/chapter1/1_12.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
@@ -10,7 +9,6 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
-	"net/url"
 	"strconv"
 	"time"
 )
@@ -24,54 +22,56 @@ const (
 	blackIndex  = 1
 )
 
+// lissajousに渡すパラメータ
+type Param struct {
+	cycles float64
+	res float64
+	size int
+	nframes int
+	delay int
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// クエリを渡す
-		lissajous(w, r.URL.Query())
+		// パラメータの初期値をセット
+		p := Param{5.0, 0.001, 100, 64, 8}
+		// クエリがセットされていた場合は上書きする
+		for k, vals := range r.URL.Query() {
+			v := vals[0]
+			switch k {
+			case "cycles":
+				p.cycles, _ = strconv.ParseFloat(v, 64)
+			case "res":
+				p.res, _ = strconv.ParseFloat(v, 64)
+			case "size":
+				p.size, _ = strconv.Atoi(v)
+			case "nframes":
+				p.nframes, _ = strconv.Atoi(v)
+			case "delay":
+				p.delay, _ = strconv.Atoi(v)
+			}
+		}
+
+		lissajous(w, p)
 	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func lissajous(out io.Writer, query url.Values) {
-	var (
-		cycles  = 5.0
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
-	)
-
-	// クエリがセットされていた場合は上書きする
-	for k, vals := range query {
-		v := vals[0]
-		switch k {
-		case "cycles":
-			cycles, _ = strconv.ParseFloat(v, 64)
-		case "res":
-			res, _ = strconv.ParseFloat(v, 64)
-		case "size":
-			size, _ = strconv.Atoi(v)
-		case "nframes":
-			nframes, _ = strconv.Atoi(v)
-		case "delay":
-			delay, _ = strconv.Atoi(v)
-		}
-	}
-
+func lissajous(out io.Writer, p Param) {
 	freq := rand.Float64() * 3.0
-	anim := gif.GIF{LoopCount: nframes}
+	anim := gif.GIF{LoopCount: p.nframes}
 	phase := 0.0
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+	for i := 0; i < p.nframes; i++ {
+		rect := image.Rect(0, 0, 2*p.size+1, 2*p.size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < p.cycles*2*math.Pi; t += p.res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5), blackIndex)
+			img.SetColorIndex(p.size+int(x*float64(p.size)+0.5), p.size+int(y*float64(p.size)+0.5), blackIndex)
 		}
 		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
+		anim.Delay = append(anim.Delay, p.delay)
 		anim.Image = append(anim.Image, img)
 	}
 	gif.EncodeAll(out, &anim)
